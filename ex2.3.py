bfafbde0b87144bf4e1d1f1204a30dbe3792c8bb
Florentin Muraille <muraillef@gmail.com>
2025-01-22 20:05:34 +0100

    ex carré et bonjour

---
+++ ex2.3.py
@@ -0,0 +1,5 @@
+# __author__ = Florentin
+# __version__ = 0.1
+
+n = int(input("Entrez un nombre : "))
+print(f"Le carré du nombre est : {n**2}")
---
+++ ex2.4.py
@@ -0,0 +1,5 @@
+# __author__ = Florentin
+# __version__ = 0.1
+
+prenom = input("Quelle est votre prénom : ")
+print(f"Bonjour, {prenom} !")
