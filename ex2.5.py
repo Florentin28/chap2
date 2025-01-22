85704ef4ddf1cd421dd961a1bbdcf779cdbf0f20
Florentin Muraille <muraillef@gmail.com>
2025-01-22 20:25:14 +0100

    ex magasin

--- ex2.5.py
+++ ex2.5.py
@@ -1,2 +1,11 @@
 # __author__ = Florentin
 # __version__ = 0.1
+
+nom_article = input("Veuillez entrer le nom de l'article : ")
+prix_htva = float(input("Veuillez entrer le prix de l'article : "))
+taux_tva = float(input("Veuillez entrer le taux de TVA (en pourcentage) : "))
+nombre_article = int(input("Veuillez entrer le nombre d'articles : "))
+
+tvac = prix_htva * (1 + taux_tva / 100) * nombre_article
+
+print(f"Le prix TVAC de '{nom_article}' est de : {tvac:.2f} €")
